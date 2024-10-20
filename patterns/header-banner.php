e45df9195424097366433e8fb5d45b4bd6288cda
Menn <m@seedwebs.com>
2024-10-20 20:15:34 +0700

    Header Banner spacing

--- patterns/header-banner.php
+++ patterns/header-banner.php
@@ -1,8 +1,10 @@
 <?php
 /**
- * Title: header-banner
+ * Title: Header with banner
  * Slug: seed/header-banner
- * Inserter: no
+ * Categories: header
+ * Block Types: core/template-part/header
+ * Description: Header with a hero banner.
  */
 ?>
 <!-- wp:group {"metadata":{"name":"Header"},"style":{"position":{"type":""},"elements":{"link":{"color":{"text":"var:preset|color|base-alt"}}},"dimensions":{"minHeight":""},"color":{"gradient":"linear-gradient(135deg,rgb(2,52,86) 2%,rgb(0,60,120) 99%)"}},"textColor":"base-alt","layout":{"inherit":"true","type":"constrained"}} -->
@@ -12,15 +14,15 @@
             <!-- wp:site-title {"level":0,"style":{"typography":{"fontStyle":"normal","fontWeight":"500"}},"fontSize":"large"} /-->
         </div>
         <!-- /wp:group -->
-        <!-- wp:navigation {"className":"is-style-outline"} /-->
+        <!-- wp:navigation {"className":"is-style-outline is-style-nav-outline"} /-->
     </div>
     <!-- /wp:group -->
-    <!-- wp:group {"metadata":{"name":"Intro"},"layout":{"type":"constrained","wideSize":"600px"}} -->
-    <div class="wp-block-group"><!-- wp:heading {"textAlign":"center","level":1,"style":{"typography":{"fontStyle":"normal","fontWeight":"500"}},"fontSize":"xx-large"} -->
+    <!-- wp:group {"metadata":{"name":"Intro"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|50"}}},"layout":{"type":"constrained","wideSize":"600px"}} -->
+    <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--50)"><!-- wp:heading {"textAlign":"center","level":1,"style":{"typography":{"fontStyle":"normal","fontWeight":"500"}},"fontSize":"xx-large"} -->
         <h1 class="wp-block-heading has-text-align-center has-xx-large-font-size" style="font-style:normal;font-weight:500">Start your site with Seed</h1>
         <!-- /wp:heading -->
-        <!-- wp:paragraph {"align":"center"} -->
-        <p class="has-text-align-center">Seed 4.0 Block Theme is your go-to solution for effortlessly creating a professional website. With the power of WordPress and Site Editor, you can quickly build and customize your site without any coding skills.</p>
+        <!-- wp:paragraph {"align":"center","style":{"typography":{"lineHeight":"1.8"}}} -->
+        <p class="has-text-align-center" style="line-height:1.8">Seed 4.0 Block Theme is your go-to solution for effortlessly creating a professional website. With the power of WordPress and Site Editor, you can quickly build and customize your site without any coding skills.</p>
         <!-- /wp:paragraph -->
         <!-- wp:buttons {"style":{"spacing":{"margin":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"flex","justifyContent":"center"}} -->
         <div class="wp-block-buttons" style="margin-top:var(--wp--preset--spacing--50);margin-bottom:var(--wp--preset--spacing--50)"><!-- wp:button {"className":"is-style-fill","style":{"spacing":{"padding":{"left":"var:preset|spacing|50","right":"var:preset|spacing|50","top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}},"color":{"gradient":"linear-gradient(180deg,rgb(49,167,236) 0%,rgb(29,147,216) 100%)"}}} -->
@@ -32,7 +34,7 @@
     <!-- /wp:group -->
     <!-- wp:group {"metadata":{"name":"Banner"},"align":"wide","layout":{"type":"constrained","contentSize":"","wideSize":"960px"}} -->
     <div class="wp-block-group alignwide"><!-- wp:image {"sizeSlug":"full","linkDestination":"none","align":"wide"} -->
-        <figure class="wp-block-image alignwide size-full"><img src="<?php echo esc_url(get_stylesheet_directory_uri()); ?>/assets/img/screen.webp" alt="" class="" /></figure>
+        <figure class="wp-block-image alignwide size-full"><img src="<?php echo esc_url(get_stylesheet_directory_uri()); ?>/assets/img/screen.webp" alt="" /></figure>
         <!-- /wp:image -->
     </div>
     <!-- /wp:group -->
