5a2166ff7a16c6e43f718bacb710a9a4961f2240
Menn <m@seedwebs.com>
2024-10-23 11:16:49 +0700

    Change Banner

--- patterns/header-banner.php
+++ patterns/header-banner.php
@@ -22,17 +22,17 @@
         <h1 class="wp-block-heading has-text-align-center has-xx-large-font-size" style="font-style:normal;font-weight:500">Start your site with Seed</h1>
         <!-- /wp:heading -->
         <!-- wp:paragraph {"align":"center","style":{"typography":{"lineHeight":"1.8"}}} -->
-        <p class="has-text-align-center" style="line-height:1.8">Seed 4.0 Block Theme is your go-to solution for effortlessly creating a professional website. With the power of WordPress and Site Editor, you can quickly build and customize your site without any coding skills.</p>
+        <p class="has-text-align-center" style="line-height:1.8">Seed 4.0 Free Block Theme is your go-to solution for effortlessly creating a professional website. With the power of WordPress and Site Editor, you can quickly build and customize your site without any coding skills.</p>
         <!-- /wp:paragraph -->
         <!-- wp:buttons {"style":{"spacing":{"margin":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"flex","justifyContent":"center"}} -->
         <div class="wp-block-buttons" style="margin-top:var(--wp--preset--spacing--50);margin-bottom:var(--wp--preset--spacing--50)"><!-- wp:button {"className":"is-style-fill","style":{"spacing":{"padding":{"left":"var:preset|spacing|50","right":"var:preset|spacing|50","top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}},"color":{"gradient":"linear-gradient(180deg,rgb(49,167,236) 0%,rgb(29,147,216) 100%)"}}} -->
-            <div class="wp-block-button is-style-fill"><a class="wp-block-button__link has-background wp-element-button" style="background:linear-gradient(180deg,rgb(49,167,236) 0%,rgb(29,147,216) 100%);padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--50)">Get start</a></div>
+            <div class="wp-block-button is-style-fill"><a class="wp-block-button__link has-background wp-element-button" href="https://github.com/SeedWebs/seed/releases/" style="background:linear-gradient(180deg,rgb(49,167,236) 0%,rgb(29,147,216) 100%);padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--50)" target="_blank" rel="noreferrer noopener nofollow">Download Seed 4.0</a></div>
             <!-- /wp:button -->
         </div>
         <!-- /wp:buttons -->
     </div>
     <!-- /wp:group -->
-    <!-- wp:group {"metadata":{"name":"Banner"},"align":"wide","layout":{"type":"constrained","contentSize":"810px","wideSize":"810px"}} -->
+    <!-- wp:group {"metadata":{"name":"Banner"},"align":"wide","layout":{"type":"constrained","contentSize":"960px","wideSize":"960px"}} -->
     <div class="wp-block-group alignwide"><!-- wp:image {"sizeSlug":"full","linkDestination":"none","align":"wide","style":{"border":{"radius":{"topLeft":"12px","topRight":"12px"}}}} -->
         <figure class="wp-block-image alignwide size-full has-custom-border"><img src="<?php echo esc_url(get_stylesheet_directory_uri()); ?>/assets/img/browser.webp" alt="" style="border-top-left-radius:12px;border-top-right-radius:12px" /></figure>
         <!-- /wp:image -->
