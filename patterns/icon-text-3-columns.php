25e54182206575aeb10721355b7741acc711432a
Menn <m@seedwebs.com>
2024-11-02 14:53:37 +0700

    More Patterns

---
+++ patterns/icon-text-3-columns.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Title: Icon & Text 3 Columns
+ * Slug: seed/icon-text-3-columns
+ * Categories: featured, about
+ */
+?>
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70"},"margin":{"top":"0","bottom":"0"}},"shadow":"var:preset|shadow|shadow"},"backgroundColor":"base-alt","layout":{"type":"constrained","contentSize":""}} -->
+<div class="wp-block-group alignwide has-base-alt-background-color has-background" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70);box-shadow:var(--wp--preset--shadow--shadow)"><!-- wp:spacer {"height":"4px","className":"is-style-desktop-only"} -->
+    <div style="height:4px" aria-hidden="true" class="wp-block-spacer is-style-desktop-only"></div>
+    <!-- /wp:spacer -->
+    <!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|60","left":"var:preset|spacing|60"}}}} -->
+    <div class="wp-block-columns alignwide"><!-- wp:column -->
+        <div class="wp-block-column"><!-- wp:image {"width":"60px","sizeSlug":"full","linkDestination":"none","align":"center"} -->
+            <figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/logo.png" alt="" class="" style="width:60px" /></figure>
+            <!-- /wp:image -->
+            <!-- wp:heading {"textAlign":"center","fontSize":"large"} -->
+            <h2 class="wp-block-heading has-text-align-center has-large-font-size"><strong>No-Code&nbsp;Builder</strong></h2>
+            <!-- /wp:heading -->
+            <!-- wp:paragraph {"align":"center","placeholder":"Content…","style":{"spacing":{"margin":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}}},"fontSize":"medium"} -->
+            <p class="has-text-align-center has-medium-font-size" style="margin-top:var(--wp--preset--spacing--20);margin-bottom:var(--wp--preset--spacing--20)"><strong>with Site Editor</strong></p>
+            <!-- /wp:paragraph -->
+            <!-- wp:paragraph {"align":"center"} -->
+            <p class="has-text-align-center">The No-Code Web Builder offers a library of ready-made Patterns and Templates, making it easy to structure, style and launch your site. </p>
+            <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:column -->
+        <!-- wp:column -->
+        <div class="wp-block-column"><!-- wp:image {"width":"60px","sizeSlug":"full","linkDestination":"none","align":"center"} -->
+            <figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/logo.png" alt="" class="" style="width:60px" /></figure>
+            <!-- /wp:image -->
+            <!-- wp:heading {"textAlign":"center","fontSize":"large"} -->
+            <h2 class="wp-block-heading has-text-align-center has-large-font-size"><strong>Intrinsic Design </strong></h2>
+            <!-- /wp:heading -->
+            <!-- wp:paragraph {"align":"center","placeholder":"Content…","style":{"spacing":{"margin":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}}},"fontSize":"medium"} -->
+            <p class="has-text-align-center has-medium-font-size" style="margin-top:var(--wp--preset--spacing--20);margin-bottom:var(--wp--preset--spacing--20)"><strong>with Seed theme</strong></p>
+            <!-- /wp:paragraph -->
+            <!-- wp:paragraph {"align":"center"} -->
+            <p class="has-text-align-center">Seed theme uses the Intrinsic Design System to create fluid typography and spacing that adjust seamlessly across all screen sizes.</p>
+            <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:column -->
+        <!-- wp:column -->
+        <div class="wp-block-column"><!-- wp:image {"width":"60px","sizeSlug":"full","linkDestination":"none","align":"center"} -->
+            <figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/logo.png" alt="" class="" style="width:60px" /></figure>
+            <!-- /wp:image -->
+            <!-- wp:heading {"textAlign":"center","fontSize":"large"} -->
+            <h2 class="wp-block-heading has-text-align-center has-large-font-size"><strong>Free &amp; Open Source</strong></h2>
+            <!-- /wp:heading -->
+            <!-- wp:paragraph {"align":"center","placeholder":"Content…","style":{"spacing":{"margin":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}}},"fontSize":"medium"} -->
+            <p class="has-text-align-center has-medium-font-size" style="margin-top:var(--wp--preset--spacing--20);margin-bottom:var(--wp--preset--spacing--20)"><strong>with 100% GPL</strong></p>
+            <!-- /wp:paragraph -->
+            <!-- wp:paragraph {"align":"center"} -->
+            <p class="has-text-align-center">Seed theme is fully open-source under the GPL license, empowering developers and creators to freely use, modify, and share.</p>
+            <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:column -->
+    </div>
+    <!-- /wp:columns -->
+    <!-- wp:spacer {"height":"4px","className":"is-style-desktop-only"} -->
+    <div style="height:4px" aria-hidden="true" class="wp-block-spacer is-style-desktop-only"></div>
+    <!-- /wp:spacer -->
+</div>
+<!-- /wp:group -->
---
+++ patterns/media-text-alternation.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Title: Media & Text Alternation
+ * Slug: seed/media-text-alternation
+ * Categories: featured, about
+ */
+?>
+<!-- wp:group {"align":"wide","className":"is-style-overflow-hidden","style":{"shadow":"var:preset|shadow|shadow","border":{"radius":"5px"},"spacing":{"blockGap":"0"}},"backgroundColor":"base-alt","layout":{"type":"constrained","contentSize":""}} -->
+<div class="wp-block-group alignwide is-style-overflow-hidden has-base-alt-background-color has-background" style="border-radius:5px;box-shadow:var(--wp--preset--shadow--shadow)"><!-- wp:media-text {"align":"wide","mediaId":256,"mediaLink":"http://seed4.local/banner-10/","mediaType":"image","verticalAlignment":"center","imageFill":true} -->
+    <div class="wp-block-media-text alignwide is-stacked-on-mobile is-vertically-aligned-center is-image-fill-element">
+        <figure class="wp-block-media-text__media"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/banner-10-1024x538.webp" alt="" class="wp-image-256 size-full" style="object-position:50% 50%" /></figure>
+        <div class="wp-block-media-text__content"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70","left":"0","right":"0"}},"dimensions":{"minHeight":""}},"layout":{"type":"constrained"}} -->
+            <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--70);padding-right:0;padding-bottom:var(--wp--preset--spacing--70);padding-left:0"><!-- wp:heading {"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"fontSize":"x-large"} -->
+                <h2 class="wp-block-heading has-x-large-font-size" style="margin-top:0;margin-bottom:0"><strong>Build a Website Without Code&nbsp;</strong></h2>
+                <!-- /wp:heading -->
+                <!-- wp:paragraph {"placeholder":"Content…","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"fontSize":"large"} -->
+                <p class="has-large-font-size" style="margin-top:0;margin-bottom:0"><strong>Using WordPress Site Editor</strong></p>
+                <!-- /wp:paragraph -->
+                <!-- wp:paragraph -->
+                <p>With the WordPress Site Editor, you can build a custom, polished website—no coding needed. The No-Code Website Builder offers a library of ready-made Patterns and Templates, making it easy to structure, style, and launch your site. Simply drag, drop, and customize elements to bring your vision to life.</p>
+                <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+        </div>
+    </div>
+    <!-- /wp:media-text -->
+    <!-- wp:media-text {"align":"wide","mediaPosition":"right","mediaId":252,"mediaLink":"http://seed4.local/banner-06/","mediaType":"image","verticalAlignment":"center","imageFill":true} -->
+    <div class="wp-block-media-text alignwide has-media-on-the-right is-stacked-on-mobile is-vertically-aligned-center is-image-fill-element">
+        <div class="wp-block-media-text__content"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70"}},"dimensions":{"minHeight":""}},"layout":{"type":"constrained"}} -->
+            <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70)"><!-- wp:heading {"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"fontSize":"x-large"} -->
+                <h2 class="wp-block-heading has-x-large-font-size" style="margin-top:0;margin-bottom:0"><strong>Intrinsic Design System</strong></h2>
+                <!-- /wp:heading -->
+                <!-- wp:paragraph {"placeholder":"Content…","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"fontSize":"large"} -->
+                <p class="has-large-font-size" style="margin-top:0;margin-bottom:0"><strong>with Seed - Block Theme</strong></p>
+                <!-- /wp:paragraph -->
+                <!-- wp:paragraph -->
+                <p>Seed theme uses the Intrinsic Design System to create fluid typography and spacing that adjust seamlessly across all screen sizes—no breakpoints needed. This adaptive approach ensures your content looks perfect on any device, letting you focus on your message without technical tweaks.</p>
+                <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+        </div>
+        <figure class="wp-block-media-text__media"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/banner-06-1024x538.webp" alt="" class="wp-image-252 size-full" style="object-position:50% 50%" /></figure>
+    </div>
+    <!-- /wp:media-text -->
+</div>
+<!-- /wp:group -->
